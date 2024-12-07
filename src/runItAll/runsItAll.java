466bf18ba5096f3b994bcabff73c701f7f3dbe3e
Lucas Mason <lucas.mason0620@gmail.com>
2024-12-07 17:12:46 -0600

    Add runItAll package and RunItAll.java to execute all sections

---
+++ src/runItAll/runsItAll.java
@@ -0,0 +1,31 @@
+package runItAll;
+
+import question1.Section1;
+import question2.Section2;
+import question3.Section3;
+import question4.Section4;
+import question5_question6.Section5and6;
+import question7.Section7;
+import question8.Section8;
+import question9.Section9;
+import question_10.Section10;
+import question_11.Section11;
+import question_12.Section12;
+import question_13.Section13;
+// Method to run all of the code at once to ensure each quesiton can run and compile
+public class runsItAll {
+    public static void main(String[] args) {
+        Section1.main(args);
+        Section2.main(args);
+        Section3.main(args);
+        Section4.main(args);
+        Section5and6.main(args);
+        Section7.main(args);
+        Section8.main(args);
+        Section9.main(args);
+        Section10.main(args);
+        Section11.main(args);
+        Section12.main(args);
+        Section13.main(args);
+    }
+}
